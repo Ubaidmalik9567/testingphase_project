d779a5c00e5c4be4e6e15581b4c0f132232f99e9
Ubaidmalik9567 <ubaidmalik9567@yahoo.com>
2024-05-03 11:35:52 +0500

    add file

--- app.py
+++ app.py
@@ -11,4 +11,18 @@
 # print(dataset.head(2))
 # dataset.rename(columns={"Unnamed: 0":"index","COL 1":"target","COL 2":"message"},inplace=True)
 # print(dataset.head(2))
-print("cicd run")
+# print("cicd run")
+from lazypredict.Supervised import LazyClassifier
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+data = load_breast_cancer()
+X = data.data
+y= data.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=.5,random_state =123)
+
+clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
+models,predictions = clf.fit(X_train, X_test, y_train, y_test)
+
+print(models)
